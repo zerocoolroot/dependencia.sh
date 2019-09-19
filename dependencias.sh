7d27288c51218f1151acaaa73b906cb95c45d15f
zerocoolroot <44586443+zerocoolroot@users.noreply.github.com>
2019-09-18 22:38:41 -0300

    Create dependencias.sh

---
+++ dependencias.sh
@@ -0,0 +1,1 @@
+sudo apt-get install build-essential libacl1-dev libattr1-dev libblkid-dev libgnutls-dev libreadline-dev python-dev libpam0g-dev python-dnspython gdb pkg-config libpopt-dev libldap2-dev dnsutils libbsd-dev docbook-xsl libcups2-dev libpam-smbpass nfs-kernel-server
